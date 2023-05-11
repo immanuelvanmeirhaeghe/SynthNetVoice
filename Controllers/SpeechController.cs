6d66942ab8d3b32fffd7f9497fa993f61b6bb6af
Immanuel Van Meirhaegghe <131171390+immanuelvanmeirhaeghe@users.noreply.github.com>
2023-05-11 08:22:01 +0200

    Added operation to get locally installed Windows TTS voices.

--- Controllers/SpeechController.cs
+++ Controllers/SpeechController.cs
@@ -4,6 +4,8 @@ using System.Speech.Synthesis;
 using System.Speech.Recognition;
 using System.Runtime.Versioning;
 using static System.Net.Mime.MediaTypeNames;
+using System.Speech.AudioFormat;
+using System.Text;
 
 namespace SynthNetVoice.Controllers
 {
@@ -34,6 +36,54 @@ namespace SynthNetVoice.Controllers
              prompt.AppendText(text);
             synthesizer.Speak(prompt);
         }
+
+        [HttpGet]
+        [Route("api/voices/installed")]
+        public string InstalledVoices()
+        {
+            // Output information about all of the installed voices.
+            var infoBuilder = new StringBuilder();
+
+            infoBuilder.AppendLine("Installed voices -");
+            foreach (InstalledVoice voice in synthesizer.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+                string AudioFormats = "";
+                foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
+                {
+                    AudioFormats += String.Format("{0}\n",
+                    fmt.EncodingFormat.ToString());
+                }
+
+                infoBuilder.AppendLine(" Name:          " + info.Name);
+                infoBuilder.AppendLine(" Culture:       " + info.Culture);
+                infoBuilder.AppendLine(" Age:           " + info.Age);
+                infoBuilder.AppendLine(" Gender:        " + info.Gender);
+                infoBuilder.AppendLine(" Description:   " + info.Description);
+                infoBuilder.AppendLine(" ID:            " + info.Id);
+                infoBuilder.AppendLine(" Enabled:       " + voice.Enabled);
+                if (info.SupportedAudioFormats.Count != 0)
+                {
+                    infoBuilder.AppendLine(" Audio formats: " + AudioFormats);
+                }
+                else
+                {
+                    infoBuilder.AppendLine(" No supported audio formats found");
+                }
+
+                string AdditionalInfo = "";
+                foreach (string key in info.AdditionalInfo.Keys)
+                {
+                    AdditionalInfo += String.Format("  {0}: {1}\n", key, info.AdditionalInfo[key]);
+                }
+
+                infoBuilder.AppendLine(" Additional Info - " + AdditionalInfo);
+                infoBuilder.AppendLine();
+            }
+
+            return infoBuilder.ToString();
+        }
+
 
     }
 
